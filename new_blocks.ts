5e47783eded57d82897547519e34c731bb266fe3
skyler1325 <168039148+skyler1325@users.noreply.github.com>
2024-04-28 14:26:45 -0600

    Initial files for MakeCode project

---
+++ new_blocks.ts
@@ -0,0 +1,17 @@
+// Add your code here
+/**
+ * Custom blocks
+ */
+
+
+
+//% weight=100 color=#1acc99 icon="\uf233"
+namespace blocks {
+    /**
+     * idk
+     */
+    //% block
+    export function low_battery (){
+        console.error("!!low battery 5% charge your microbit!!")
+    }
+}
