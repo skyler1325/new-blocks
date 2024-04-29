be10c552aee6dc178884e36b11a43672eacca676
skyler1325 <168039148+skyler1325@users.noreply.github.com>
2024-04-29 08:38:15 -0600

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+print(":) hello this is the new blocks extension")
--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-input.onButtonPressed(Button.AB, function () {
-    show = !(show)
-})
-let show = false
-show = true
-basic.forever(function () {
-    if (show) {
-        led.toggle(randint(0, 4), randint(0, 4))
-        led.setBrightness(60)
-    }
-    if (!(show)) {
-        basic.clearScreen()
-    }
-})
+console.log(":) hello this is the new blocks extension")
